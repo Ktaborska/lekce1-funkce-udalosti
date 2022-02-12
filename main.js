afefb386d06a7ea1f6a12308e1a6d84c534483e5
Kateřina Táborská <ktaborska92@gmail.com>
2022-02-12 22:32:33 +0100

    vypracované úkoly, nesplněný poslední bod

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-// tady je místo pro náš program
+//onclick="secti(4, 5)"
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
-
-/**
- * Sečte dvě čísla na vstupu a vrátí výsledek
- *
- * @param {int} a
- * @param {int} b
- */
 function secti(a, b) {
-  let c = a + b;
-  return c;
+  document.querySelector("#vysledek").innerHTML = a+b
 }
 
-/**
- * Upozorní uživatele při spuštění.
- */
-function upozorni() {
+//let c = a + b;
+//return c;
+// document.querySelector("#vysledek").innerHTML = c
+
+//document.querySelector("#vysledek").innerHTML = secti(4, 5);
+
+/* Upozorní uživatele při spuštění.*/
+
+// onclick="upozorni('.ctverecek','green')"
+function upozorni(className, color) {
   alert("Gratulace, právě jsi spustila tuto funkci!");
-}
+  document.querySelector(className).style.backgroundColor = color;
+  console.log('Gratulace, právě jsi spustila tuto funkci!')
+  document.querySelector(className).innerHTML = 'Gratulace, právě si spustila tuto funkci!';
+  className.style.width = '400px';
+  }
+
+  /*
+  let ctverecek = document.querySelector(className);
+  square.style.backgroundColor =color;
+  */
+
+
+
+
