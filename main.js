433de2151c01b0d6cc4e27b2b9ffee0515fef57f
Kateřina Táborská <ktaborska92@gmail.com>
2022-02-13 15:33:05 +0100

    dodělání

--- main.js
+++ main.js
@@ -4,32 +4,23 @@ function secti(a, b) {
   document.querySelector("#vysledek").innerHTML = a+b
 }
 
-//let c = a + b;
-//return c;
+// let c = a + b;
+// return c;
 // document.querySelector("#vysledek").innerHTML = c
-
 //document.querySelector("#vysledek").innerHTML = secti(4, 5);
 
-/* Upozorní uživatele při spuštění.*/
 
 // onclick="upozorni('.ctverecek','green')"
+
 function upozorni(className, color) {
   alert("Gratulace, právě jsi spustila tuto funkci!");
   document.querySelector(className).style.backgroundColor = color;
   }
 
+//onclick="hlaska('.ctverecek', '400')
+
 function hlaska(className, width){
   console.log('Gratulace, právě jsi spustila tuto funkci!')
   document.querySelector(className).innerHTML = 'Gratulace, právě si spustila tuto funkci!';
   document.querySelector(className).style.width = width + 'px';
-
 }
-
-  /*
-  let ctverecek = document.querySelector(className);
-  square.style.backgroundColor =color;
-  */
-
-
-
-
